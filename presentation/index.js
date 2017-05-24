13207cfc4230392652adfbdf2a72c372c517a3a2
Kyle Welch <kwelch0626@gmail.com>
2017-05-23 21:34:33 -0500

    finished intro
    update to eslint file

--- presentation/index.js
+++ presentation/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Import Spectacle Core tags
-import { BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Slide, Text } from "spectacle";
+import { Appear, Markdown, Deck, Image, Heading, ListItem, List, Slide, Text, Layout, Fill } from "spectacle";
 
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
@@ -14,7 +14,10 @@ import createTheme from "spectacle/lib/themes/default";
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
-const images = {};
+const images = {
+  twitterLogo: require("../assets/twitter-logo.svg"),
+  githubLogo: require("../assets/GitHub-Mark-120px-plus.png"),
+};
 
 preloader(images);
 
@@ -23,25 +26,70 @@ const theme = createTheme(
     primary: "white",
     secondary: "#1F2022",
     tertiary: "#03A9FC",
-    quartenary: "#CECECE"
+    quartenary: "#CECECE",
   },
   {
     primary: "Montserrat",
-    secondary: "Helvetica"
+    secondary: "Helvetica",
   }
 );
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme} progress="pacman">
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            Introduction to<br />Component Based Styling
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="5rem 0 0" textColor="tertiary" size={3} bold>
+            Kyle Welch
           </Text>
+          <Text margin=".5rem 0 0" textColor="secondary" size={0.75} italic>
+            Sr. Software Development Engineer @ NFIB
+          </Text>
+          <Layout style={{ marginTop: 100, justifyContent: "space-between" }}>
+            <Fill>
+              <Text textColor="tertiary" style={{ textAlign: "left" }}>
+                <Image src={images.twitterLogo} style={{ height: 25, margin: "0 10px 0" }} />
+                @kylewelch
+              </Text>
+            </Fill>
+            <Fill>
+              <Text style={{ textAlign: "right" }}>
+                <Image src={images.githubLogo} style={{ height: 30, margin: "0 10px 0" }} />
+                /kwelch
+              </Text>
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide
+          transition={["fade"]}
+          bgColor="quartenary"
+          notes={`<ul>
+            <li>I would love to talk to all of you. Find me, we will talk ;)</li>
+            <li>There is no silver bullet, be open to any solution</li>
+          </ul>`}
+        >
+          <Heading fit textColor="secondary">What this talk is NOT?</Heading>
+          <List style={{ listStyleType: "none" }}>
+            <Appear><ListItem>- Q&A</ListItem></Appear>
+            <Appear><ListItem>- Declaring a champion</ListItem></Appear>
+          </List>
+        </Slide>
+        <Slide
+          transition={["fade"]}
+          bgColor="primary"
+          notes={`<ul>
+          <li>History of styling</li>
+          <li>Compare and learn their advantages<li>
+        </ul>`}
+        >
+          <Heading fit textColor="tertiary">What is this talk?</Heading>
+          <List style={{ listStyleType: "none" }}>
+            <Appear><ListItem>- Overview of styling over time</ListItem></Appear>
+            <Appear><ListItem>- Comparision of new styling approaches</ListItem></Appear>
+          </List>
         </Slide>
       </Deck>
     );
